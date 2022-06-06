acd920187a4901c214c0cfe6e9af33c735d2acf0
tuanteng96 <tuantengyn@gmail.com>
2022-06-06 23:25:38 +0700

    first commit

--- src/features/Reports/pages/RP-ReEx/index.js
+++ src/features/Reports/pages/RP-ReEx/index.js
@@ -19,11 +19,11 @@ const JSONData = {
   DS_CHI_Week: [
     38000000, 11000000, 17000000, 51000000, 32000000, 8500000, 14000000
   ],
-  TonTienDauKy: 12000000,
+  TonTienDauKy: 10000000,
   ThuTrongKy: 12000000,
-  ChiTrongKy: 12000000,
+  ChiTrongKy: 11000000,
   ThuChiTrongKy: 6000000,
-  ThuChiHienTai: 12000000
+  ThuChiHienTai: 15000000
 }
 
 const optionsObj = {
@@ -172,17 +172,32 @@ function RPReEx(props) {
         </div>
       </div>
       <div className="row">
-        <div className="col-md-3">
-          <div className="bg-white rounded p-15px">
-            <div>{PriceHelper.formatVND(OverviewData?.TonTienDauKy)}</div>
+        <div className="col-md-6 order-1">
+          <div className="bg-white rounded px-20px py-30px text-center" style={{ backgroundPosition: 'right top', backgroundSize: '30% auto', backgroundRepeat: 'no-repeat', backgroundImage: 'url(https://preview.keenthemes.com/metronic/theme/html/demo1/dist/assets/media/svg/shapes/abstract-4.svg)' }}>
+            <div className="font-number font-size-35 fw-600 line-height-xxl text-primary">{PriceHelper.formatVND(OverviewData?.ThuChiHienTai)}</div>
+            <div className="fw-600 text-uppercase text-muted font-size-smm mt-5px">Tồn tiền đầu kỳ</div>
           </div>
         </div>
-        <div className="col-md-6">
-          <div className="bg-white rounded">a</div>
+        <div className="col-md-6 order-3 order-md-2">
+          <div className="bg-white rounded px-20px py-30px mt-20px mt-md-0 text-center" style={{ backgroundPosition: 'right top', backgroundSize: '30% auto', backgroundRepeat: 'no-repeat', backgroundImage: 'url(https://preview.keenthemes.com/metronic/theme/html/demo1/dist/assets/media/svg/shapes/abstract-4.svg)' }}>
+            <div className="font-number font-size-35 fw-600 line-height-xxl text-success">{PriceHelper.formatVND(OverviewData?.TonTienDauKy)}</div>
+            <div className="fw-600 text-uppercase text-muted font-size-smm mt-5px">Tồn tiền hiện tại</div>
+          </div>
         </div>
-        <div className="col-md-3">
-          <div className="bg-white rounded p-15px">
-            <div>{PriceHelper.formatVND(OverviewData?.TonTienDauKy)}</div>
+        <div className="col-md-12 order-2 order-md-3">
+          <div className="bg-white rounded px-20px py-30px d-flex mt-20px flex-column flex-md-row">
+            <div className="flex-1 text-center">
+              <div className="font-number font-size-30 fw-600 line-height-xxl">{PriceHelper.formatVND(OverviewData?.ThuTrongKy)}</div>
+              <div className="fw-600 text-uppercase text-muted font-size-smm mt-5px">Thu trong kỳ</div>
+            </div>
+            <div className="flex-1 text-center border-left border-left-0 border-md-left-1  border-right border-right-0 border-md-right-1 border-gray-200 border-bottom border-md-bottom-0 border-top border-md-top-0 py-20px my-20px py-md-0 my-md-0">
+              <div className="font-number font-size-30 fw-600 line-height-xxl text-danger">{PriceHelper.formatVND(OverviewData?.ChiTrongKy)}</div>
+              <div className="fw-600 text-uppercase text-muted font-size-smm mt-5px">Chi trong kỳ</div>
+            </div>
+            <div className="flex-1 text-center">
+              <div className="font-number font-size-30 fw-600 line-height-xxl">{PriceHelper.formatVND(OverviewData?.ThuChiTrongKy)}</div>
+              <div className="fw-600 text-uppercase text-muted font-size-smm mt-5px">Tồn kỳ</div>
+            </div>
           </div>
         </div>
       </div>
