ce2f6fc412dc2b4e582ed515e20c2fde8a0957ec
tuanteng96 <tuantengyn@gmail.com>
2025-03-20 10:37:12 +0700

    first commit

--- src/helpers/PriceHelper.js
+++ src/helpers/PriceHelper.js
@@ -1,9 +1,19 @@
 export const PriceHelper = {
+  countDecimals: value => {
+    if (Math.floor(value) === value) return 0
+    return value.toString().split('.')[1].length || 0
+  },
   formatVND: price => {
     if (!price || price === 0) {
       return '0'
+    } else if (Number(price) % 1 !== 0) {
+      return Number(price)
+        .toFixed(PriceHelper.countDecimals(Number(price)))
+        .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
     } else {
-      return price.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.')
+      return Number(price)
+        .toFixed(0)
+        .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.')
     }
   },
   formatVNDPositive: price => {
