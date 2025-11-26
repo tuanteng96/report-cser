5214bc1939d524b52d29647c0c4760eeb0f5a32f
tuanteng96 <tuantengyn@gmail.com>
2025-11-26 12:23:36 +0700

    update

--- src/helpers/PriceHelper.js
+++ src/helpers/PriceHelper.js
@@ -1,20 +1,19 @@
 export const PriceHelper = {
   countDecimals: value => {
-    if (Math.floor(value) === value) return 0
-    return value.toString().split('.')[1].length || 0
+    if (Math.floor(value) === value) return 0;
+    return value.toString().split('.')[1].length || 0;
   },
   formatVND: price => {
-    if (!price || price === 0) {
-      return '0'
-    } else if (Number(price) % 1 !== 0) {
-      return Number(price)
-        .toFixed(PriceHelper.countDecimals(Number(price)))
-        .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
-    } else {
-      return Number(price)
-        .toFixed(0)
-        .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.')
-    }
+    if (!price || price === 0) return '0';
+
+    let decimals = PriceHelper.countDecimals(Number(price));
+    let parts = Number(price).toFixed(decimals).split('.');
+
+    // Thêm dấu chấm ngăn cách nghìn
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+    // Nối lại phần thập phân nếu có
+    return parts.length > 1 ? parts[0] + ',' + parts[1] : parts[0];
   },
   formatVNDPositive: price => {
     if (!price || price === 0) {
